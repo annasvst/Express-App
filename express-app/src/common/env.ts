dcdeb6bd529115339f2ba9e5708e93e27c5a6533
mugeylmz7 <mugeylmz1994@gmail.com>
2026-08-05 13:17:56 +0300

    refactor: update environment variable handling for MongoDB and server configuration

--- express-app/src/common/env.ts
+++ express-app/src/common/env.ts
@@ -1,3 +1,7 @@
 import dotenv from 'dotenv'
 
 dotenv.config()
+
+export const PORT = process.env.PORT || 8000
+export const MONGODB_URI = process.env.MONGODB_URI || process.env.MONGO_URI || ''
+export const MONGODB_DB_NAME = process.env.MONGODB_DB_NAME || ''
--- express-app/src/server.ts
+++ express-app/src/server.ts
@@ -6,9 +6,8 @@ import mongodb from './services/db'
 // to use env variables
 import './common/env'
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
   logger.info(`up and running in ${process.env.NODE_ENV || 'development'} @: ${os.hostname()} on port ${PORT}`)
-  mongodb.Connection
 })
